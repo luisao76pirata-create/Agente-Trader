9c148d02e852ae4cdf82ea6fc77f888ed79c77cb
luisao76pirata-create <luisao76.pirata@gmail.com>
2026-03-12 13:51:36 +0100

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,23 +1,15 @@
-import axios from "axios"
-
-export async function scanMarket() {
-
-    const url =
-    "https://api.dexscreener.com/latest/dex/search?q=solana"
-
-    const response = await axios.get(url)
-
-    const pairs = response.data.pairs || []
-
-    return pairs
-        .filter(p => p.chainId === "solana")
-        .filter(p => p.liquidity?.usd > 50000)
-        .filter(p => p.volume?.h24 > 100000)
-        .map(p => ({
-            token: p.baseToken.symbol,
-            price: Number(p.priceUsd),
-            liquidity: p.liquidity.usd,
-            volume24h: p.volume.h24,
-            change1h: p.priceChange?.h1 || 0
-        }))
+{
+ token
+ address
+ price
+ mcap
+ liquidity
+ ratio
+ v5m
+ momentum
+ uniqueBuyers5m
+ mintAuthority
+ freezeAuthority
+ lpLocked
+ top10
 }
